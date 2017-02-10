67b30ee500b6e5639fddc6d8793c6d0e3eed31fd
Oettinger, Matt <Matt.Oettinger@teamaol.com>
2017-02-09 23:38:06 -0500

    Initial commit

---
+++ assets/js/scripts.js
@@ -0,0 +1,33 @@
+jQuery(function($) {
+
+  // equal heights function
+  $.fn.equalHeights = function() {
+    var maxHeight = 0,
+    $this = $(this);
+    $this.each( function() {
+      var height = $(this).innerHeight();
+      if ( height > maxHeight ) { maxHeight = height; }
+    });
+    return $this.css('height', maxHeight);
+  };
+
+  // web font loader
+  WebFontConfig = {
+    custom: {
+      families: ['museo_sans300', 'museo_sans500', 'museo_sans500italic', 'museo_sans700', 'museo_sans900', 'museo_sans900italic'],
+      urls: ['../style.css']
+    }
+  };
+
+  var query = Modernizr.mq('(min-width: 768px)');
+  console.log('flex');
+   if (query) {
+     $('.flexslider').flexslider({
+       slideshow: true,
+       selector: ".slides > article",
+       controlNav: false
+     });
+   }
+
+});
+
